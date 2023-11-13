f1db1669a8853c09cb9496031a50f0f47e42bb50
Alex Young <zitayeung596@gmail.com>
2023-11-13 23:01:45 +0000

    “update”

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,33 +1,50 @@
-class GamesController < ApplicationController
-  require 'net/http'
-  require 'json'
+require 'json'
+require 'open-uri'
 
+class GamesController < ApplicationController
   def new
-    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+    alphabet = ('A'..'Z')
+    alphabet_array = alphabet.to_a
+    @letters = alphabet_array.sample(10)
   end
 
   def score
-    @word = params[:word]
-    grid = params[:word].split("")
-    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    result = JSON.parse(response)
+    @letters = params[:letters].downcase.split
+    @attempt = params[:attempt].downcase
 
-    if !word_in_grid?(@word, grid)
-      @result = "Sorry but #{@word} can't be built out of #{@letters}"
-    elsif !result["valid"]
-      @result = "Sorry but #{@word} does not seem to be a valid English word..."
+    if is_english_word? && is_valid_word?
+      @score = @attempt.length.fdiv(@letters.length) * 100
     else
-      @result = "Congratulations! #{@word} is a valid English word."
+      @score = 0
     end
+
+    @message = create_message
   end
 
   private
 
-  def word_in_grid?(word, grid)
-    word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
+  def is_english_word?
+    url = "https://wagon-dictionary.herokuapp.com/#{@attempt}"
+    api_content = URI.open(url).read
+    response = JSON.parse(api_content)
+    response["found"]
+  end
+
+  def is_valid_word?
+    @attempt.chars.all? do |char|
+      @attempt.count(char) <= @letters.count(char)
+    end
+  end
+
+  def create_message
+    if is_english_word? && is_valid_word?
+      "Congratulations! #{@attempt} is a valid English word."
+    elsif is_english_word? && !is_valid_word?
+      "Sorry but #{@attempt} can't be built out of #{@letters}"
+    elsif !is_english_word? && is_valid_word?
+      "Sorry but #{@attempt} does not seem to be a valid English word..."
+    else
+      "Sorry but #{@attempt} can't be built out of #{@letters}"
+    end
   end
 end
-
-# app/views/games/new.html.erb.
